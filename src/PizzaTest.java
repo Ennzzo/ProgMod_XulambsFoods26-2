2f9de7e3178c37243b0bb8724d4f3197adefbb59
Enzzo <enzzobambirra@gmail.com>
2026-09-11 22:05:03 -0300

    Testes Pizza

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -10,11 +10,44 @@ public class PizzaTest {
         Pizza pizza = new Pizza();
 
         //Act
-        int quantos = 
-            pizza.adicionarIngredientes(4);
+        int quantos = pizza.adicionarIngredientes(4);
 
         //Assert
         assertEquals(4, quantos);
     }
+
+    @Test
+    public void testaSePodeAdicionarIngreidente() {
+        //Arrange
+        Pizza pizza1 = new Pizza();
+        //Act
+        int quantos = pizza1.adicionarIngredientes(2);
+
+        //Assert
+        assertEquals(9, quantos);
+    }
+
+    @Test
+    public void naoAdicionaIngredientesNegativo() {
+        //Arrange
+        Pizza pizza1 = new Pizza();
+        //Act
+        int quantos = pizza1.adicionarIngredientes(-5);
+
+        //Assert
+        assertEquals(4, quantos);
+    }
+
+    @Test
+    public void calculaValorDaPizzaComAdicionais() {
+        
+        //Act
+        double valor = pizza1.valorFinal();
+
+        //Assert
+        assertEquals(49, valor, 0.01);
+    }
+
+
     
 }
